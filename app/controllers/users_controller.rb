2b507e860333a4a116147a11874e791667c7f583
adivik2000@hotmail.com <adivik2000@hotmail.com>
2016-09-02 13:16:31 +0000

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,32 +24,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-  def cancel_subscription
-    @user = User.find_by(chargebee_id: params[:id])    
-    @user.subscription.cancel({ end_of_term: params[:end_of_term] })
-    redirect_to customers_path
-  end
-  
-  def change_subscription
-    @user = User.find(params[:id])
-    @user.update_subscription(plan_id: params[:plan_id], coupon: params[:coupon_id])
-    redirect_to customers_path
-  end
-  
-  def add_card
-    @user = User.find(params[:id])
-    result = ChargeBee::Card.update_card_for_customer(user.chargebee_id, {
-      :gateway => "chargebee", 
-      :first_name => "Richard", 
-      :last_name => "Fox", 
-      :number => "4012888888881881", 
-      :expiry_month => 10, 
-      :expiry_year => 2022, 
-      :cvv => "999"
-    })
-  end
-  
-  
   
   private
     # Never trust parameters from the scary internet, only allow the white list through.
